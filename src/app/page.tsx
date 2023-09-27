727fd9604ef5da6882a46b8ded23c0e9aa6c63c4
excel007 <eak.ekkachai@gmail.com>
2023-09-28 01:02:54 +0700

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (<div>
       <h1>Hello NextJS </h1>
+      <hr />
       <li><Link href="/about">about page</Link></li>
       <li><Link href="/about/profile">profile page</Link></li>
       <li><Link href="/blog">blog page</Link></li>
